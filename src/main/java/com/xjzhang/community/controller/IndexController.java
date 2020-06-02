be3541b540447e7f2669e990b0e6b95d850e940c
xjzhang9 <15155157296@163.com>
2020-06-02 17:59:59 +0800

    1、增加了分页信息。

--- src/main/java/com/xjzhang/community/controller/IndexController.java
+++ src/main/java/com/xjzhang/community/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.xjzhang.community.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xjzhang.community.entry.dto.QuestionDto;
 import com.xjzhang.community.entry.model.User;
 import com.xjzhang.community.service.IPublishService;
@@ -7,6 +9,8 @@ import com.xjzhang.community.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -24,25 +28,48 @@ public class IndexController {
     @Autowired
     private IPublishService publishService;
 
+    private static int startPage = 1;
+    private  static int endPage = 5;
+
     @GetMapping("/")
-    public String index(HttpServletRequest req) {
-        Cookie[] cookies =  req.getCookies();
+    public String index(@RequestParam(name = "pageIndex", defaultValue = "1") int pageIndex, @RequestParam(name = "pageSize", defaultValue = "10") int pageSize, @RequestParam(name = "prev", defaultValue = "false") boolean prev, @RequestParam(name = "next", defaultValue = "false") boolean next, HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
         if (cookies != null) {
-            for (Cookie cookie: cookies) {
+            for (Cookie cookie : cookies) {
                 String cookieName = cookie.getName();
-              if ("token".equals(cookieName)) {
-                  String token = cookie.getValue();
-                  User user = userService.selectUserByToken(token);
-                  // 通过session返回用户信息，前端展示
-                  HttpSession httpSession = req.getSession();
-                  httpSession.setAttribute("userinfo", user);
-                  if (user != null) {
-                      List<QuestionDto> questionDtoList = publishService.selectQuestion();
-                      httpSession.setAttribute("questions", questionDtoList);
-                  }
-              }
+                if ("token".equals(cookieName)) {
+                    String token = cookie.getValue();
+                    User user = userService.selectUserByToken(token);
+                    // 通过session返回用户信息，前端展示
+                    HttpSession httpSession = req.getSession();
+                    httpSession.setAttribute("userinfo", user);
+                    if (user != null) {
+                        //  创建Page对象，将page，limit参数传入，必须位于从数据库查询数据的语句之前，否则不生效
+                        PageHelper.startPage(pageIndex, pageSize);
+                        List<QuestionDto> questionDtoList = publishService.selectQuestion();
+                        PageInfo<QuestionDto> questionDtoPageInfo = new PageInfo<>(questionDtoList);
+
+                        httpSession.setAttribute("questions", questionDtoPageInfo);
+
+
+
+                        httpSession.setAttribute("startPage", startPage);
+                        httpSession.setAttribute("endPage", endPage);
+
+                        if (prev & startPage > 1) {
+                            httpSession.setAttribute("startPage", startPage - 1);
+                            httpSession.setAttribute("endPage", endPage - 1);
+                        }
+
+                        if (next && endPage < questionDtoPageInfo.getPages()) {
+                            httpSession.setAttribute("startPage", startPage + 1);
+                            httpSession.setAttribute("endPage", endPage + 1);
+                        }
+                    }
+                }
             }
         }
+
         return "index";
     }
 }
--- src/main/java/com/xjzhang/community/entry/dto/QuestionDto.java
+++ src/main/java/com/xjzhang/community/entry/dto/QuestionDto.java
@@ -7,6 +7,8 @@ import com.xjzhang.community.entry.model.User;
  * @author xjzhang
  */
 public class QuestionDto extends Question {
+    private int qid;
+
     private User user;
 
     public User getUser() {
@@ -15,6 +17,14 @@ public class QuestionDto extends Question {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public int getQid() {
+        return qid;
+    }
+
+    public void setQid(int qid) {
+        this.qid = qid;
     }
 
     @Override
